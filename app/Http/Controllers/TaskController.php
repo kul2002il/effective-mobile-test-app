8adda2df4a7d6ccc5ec333d72456fec75985244e
ilia <kul.2002.il@gmail.com>
2025-11-04 15:25:14 +0700

    Create docs

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -9,13 +9,29 @@ use App\Models\Task;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Response;
 
+/**
+ * @group Task
+ *
+ * API for manage tasks.
+ */
 class TaskController extends Controller
 {
+    /**
+     * Show all tasks
+     *
+     * @return JsonResource
+     */
     public function index(): JsonResource
     {
         return TaskResource::collection(Task::all());
     }
 
+    /**
+     * Create task
+     *
+     * @param StoreTaskRequest $request
+     * @return JsonResource
+     */
     public function store(StoreTaskRequest $request): JsonResource
     {
         /** @phpstan-ignore-next-line argument.type */
@@ -24,11 +40,24 @@ class TaskController extends Controller
         return new TaskResource($task);
     }
 
+    /**
+     * Show one task
+     *
+     * @param Task $task
+     * @return JsonResource
+     */
     public function show(Task $task): JsonResource
     {
         return new TaskResource($task);
     }
 
+    /**
+     * Update task
+     *
+     * @param UpdateTaskRequest $request
+     * @param Task $task
+     * @return JsonResource
+     */
     public function update(UpdateTaskRequest $request, Task $task): JsonResource
     {
         /** @phpstan-ignore-next-line argument.type */
@@ -37,6 +66,12 @@ class TaskController extends Controller
         return new TaskResource($task);
     }
 
+    /**
+     * Delete task
+     *
+     * @param Task $task
+     * @return Response
+     */
     public function destroy(Task $task): Response
     {
         $task->delete();
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,19 @@
 
 namespace Tests;
 
+use AjCastro\ScribeTdd\Tests\ScribeTddSetup;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use RefreshDatabase;
+    use ScribeTddSetup;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->setUpScribeTdd();
+    }
 }
